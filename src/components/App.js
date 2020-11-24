cfc088e529e2df5c2c466b6b7decb1d9a6a9c2e7
GalZi <galzi123@gmail.com>
2020-11-25 00:18:28 +0200

    Added basic routing, still gets a warning and no connection to server

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import {Route, BrowserRouter, Redirect} from "react-router-dom";
 import logo from '../resources/logo.svg';
 import '../styles/App.css';
 import Title from './Title';
 import TopNavBar from './TopNavBar';
+import About from './About';
+import Shows from './Shows';
+import Media from './Media';
+import Contact from './Contact';
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
+  const [currentRoute, setCurrentRoute] = useState("/");
 
   useEffect(() => {
     fetch('/time').then(res => res.json()).then(data => {
@@ -16,7 +22,38 @@ function App() {
   return (
     <div className="App">
       <Title/>
-      <TopNavBar/>
+      <TopNavBar currentRoute={currentRoute}/>
+      <BrowserRouter basename="/">
+        <Route exact path="/" render={
+          () => {
+            return <Redirect to="/about"/>
+          }
+        }></Route>
+        <Route exact path="/about" render={
+          () => {
+            setCurrentRoute("/about");
+            return <About/>
+          }
+        }></Route>
+        <Route exact path="/shows" render={
+          () => {
+            setCurrentRoute("/shows");
+            return <Shows/>
+          }
+        }></Route>
+        <Route exact path="/media" render={
+          () => {
+            setCurrentRoute("/media");
+            return <Media/>
+          }
+        }></Route>
+        <Route exact path="/contact" render={
+          () => {
+            setCurrentRoute("/contact");
+            return <Contact/>
+          }
+        }></Route>
+      </BrowserRouter>
       {/* <header className="App-header">
         <p>The current time is {currentTime}.</p>
       </header> */}
